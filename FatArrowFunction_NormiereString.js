5f358156c8cbd406043049fd4f8b1f64cfd7a486
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-10-22 21:59:29 +0200

    Update FatArrowFunction_NormiereString.js

--- FatArrowFunction_NormiereString.js
+++ FatArrowFunction_NormiereString.js
@@ -1,25 +1,25 @@
 
 console.log();
 
-const stringNormieren1 = (eingabeString) => {
+const normiereString1 = (eingabeString) => {
 
     return eingabeString.trim().toLowerCase();
 }
 
 /**
- * Wenn die Funktion nur aus einer Anweisung besteht, und die Anweisung
- * ein Wert zurück gibt, dann können wir die Schweifenklammer und
- * das "return" weglassen.
+ * Wenn die Funktion nur aus einer Anweisung besteht, und diese Anweisung
+ * ein Wert zurück gibt, dann können wir die Schweifenklammer und das
+ * "return" weglassen.
  */ 
-const stringNormieren2 = (eingabeString) => eingabeString.trim().toLowerCase();
+const normiereString2 = (eingabeString) => eingabeString.trim().toLowerCase();
 
 
 const einString = " aBcDEF    ";
 
-const stringNormiert1 = stringNormieren1(einString);
+const stringNormiert1 = normiereString1(einString);
 console.log(`Normierter String: \"${stringNormiert1}\"`);
 
-const stringNormiert2 = stringNormieren2(einString);
+const stringNormiert2 = normiereString2(einString);
 console.log(`Normierter String: \"${stringNormiert2}\"`);
 
 console.log();
