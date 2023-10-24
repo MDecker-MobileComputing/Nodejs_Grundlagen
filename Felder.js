ecb9383a8b0c0ce91c1215c31d15adcc8af4a6af
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-10-24 21:26:27 +0200

    Felder.js: includes

--- Felder.js
+++ Felder.js
@@ -14,3 +14,11 @@ meinArray.forEach(element => {
 });
 
 console.log();
+
+const includesDuale = meinArray.includes( "Duale");
+console.log("Array enthält \"Duale\" als Element: " + includesDuale);
+
+const includesHeidelberg = meinArray.includes( "Heidelberg");
+console.log("Array enthält \"Heidelberg\" als Element: " + includesHeidelberg);
+
+console.log();
