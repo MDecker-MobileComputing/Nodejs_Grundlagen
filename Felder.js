941554c566932d04e76668085ee818a22520f659
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-10-24 21:01:23 +0200

    Felder.js verbessert

--- Felder.js
+++ Felder.js
@@ -1,9 +1,10 @@
 
 console.log();
 
-const meinArray = [ "Duale", "Hochschule", "BW"];
+const meinArray = [ "Duale", "Hochschule", "BW" ];
 
 meinArray[2] = "Baden-Württemberg"; // geht, obwohl "meinArray" mit "const" deklariert ist
+
 meinArray.push("Karlsruhe"); // am Ende des Arrays anhängen
 
 meinArray.forEach(element => {
