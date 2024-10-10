77c2d161a53ddf591229155572df94e40b98eb2c
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-10-10 09:17:36 +0200

    Create Student.js

---
+++ OOP2/Student.js
@@ -0,0 +1,62 @@
+/**
+ * Für die Matrikelnummer gibt es nur einen Getter, aber keinen Setter.
+ */
+class Student {
+
+    constructor(matrikelnummer, vorname, nachname) {
+
+        this._matrikelnummer = matrikelnummer;
+        this._vorname        = vorname;
+        this._nachname       = nachname;
+    }
+
+
+    get matrikelnummer() {
+
+        return this._matrikelnummer;
+    }
+
+
+    get vorname() {
+
+        return this._vorname;
+    }
+
+    set vorname(vorname) {
+
+        this._vorname = vorname;
+    }
+
+
+    get nachname() {
+
+        return this._nachname;
+    }
+
+    set nachname(nachname) {
+
+        this._nachname = nachname;
+    }
+
+
+    getVollerName() {
+
+        return `${this._vorname} ${this._nachname}`;
+    }
+
+    toString() {
+
+            return `Matrikelnummer: ${this._matrikelnummer}, Vorname: ${this._vorname}, Nachname: ${this._nachname}`;
+    }
+
+}
+
+
+const student1 = new Student( 123456, "Max", "Mustermann" );
+
+console.log( student1.toString() );
+
+console.log( `Vorname Student vor Änderung: ${student1.vorname}` );
+student1.vorname = "Maximilian";
+
+console.log( student1.toString() );
