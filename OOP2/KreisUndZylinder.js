17875e6173a4cad6ecdaea89e76315b826d656a9
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-10-10 09:04:30 +0200

    Kreis.js umbenannt in KreisUndZylinder.js

---
+++ OOP2/KreisUndZylinder.js
@@ -0,0 +1,53 @@
+class Kreis {
+
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    getUmfang() {
+        const umfang = 2 * Math.PI * this.radius;
+        return this._runden(umfang);
+    }
+
+    getFlaeche() {
+        const flaeche = Math.PI * Math.pow(this.radius, 2);
+        return this._runden(flaeche);
+    }
+
+    _runden(wert)  {
+        return Math.trunc(wert * 100) / 100;
+    }
+}
+
+
+const kreis1 = new Kreis(5);
+
+console.log(`Radius von Kreis: ${kreis1.radius}`);
+console.log(`Umfang von Kreis: ${kreis1.getUmfang()}`);
+console.log(`Fläche von Kreis: ${kreis1.getFlaeche()}`);
+console.log();
+
+
+/* ********************* Ab hier: Vererbung ********************* */
+
+class Zylinder extends Kreis {
+
+    constructor(radius, hoehe) {
+        super(radius);
+        this.hoehe = hoehe;
+    }
+
+    getVolumen() {
+        const volumen = this.getFlaeche() * this.hoehe;
+        return this._runden(volumen);
+    }
+
+    getOberflaeche() {
+        const oberflaeche = 2 * this.getFlaeche() + this.getUmfang() * this.hoehe;
+        return this._runden(oberflaeche);
+    }
+}
+
+const zylinder1 = new Zylinder(5, 10);
+console.log(`Volumen von Zylinder   : ${zylinder1.getVolumen()}`);
+console.log(`Oberfläche von Zylinder: ${zylinder1.getOberflaeche()}`);
