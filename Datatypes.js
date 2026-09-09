7acc26fc6b7f2430a63004951a0ead99938c8304
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-09-09 12:30:52 +0200

    Normalize typeof spacing and cleanup

--- Datatypes.js
+++ Datatypes.js
@@ -6,33 +6,29 @@
 console.log();
 
 let myVariable;
-let description = typeof(myVariable);
+let description = typeof( myVariable );
 console.log( myVariable + ": " + description );
 
 myVariable = 42;
-description = typeof(myVariable);
+description = typeof( myVariable );
 console.log( myVariable + ": " + description );
 
 myVariable = 3.14;
-description = typeof(myVariable);
+description = typeof( myVariable );
 console.log( myVariable + ": " + description );
 
 myVariable = "DHBW KA";
-description = typeof(myVariable);
+description = typeof( myVariable );
 console.log( myVariable + ": " + description );
 
 myVariable = [ 1, 2, 3 ]; // Array
-description = typeof(myVariable);
+description = typeof( myVariable );
 console.log( myVariable + ": " + description );
 
 myVariable = { firstName: "Hans", lastName: "Wiwi" }; // JSON!
-description = typeof(myVariable);
+description = typeof( myVariable );
 console.log( myVariable + ": " + description );
 
 myVariable = null;
-description = typeof(myVariable);  // also "object" for backward compatibility, see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/null
+description = typeof( myVariable );  // also "object" for backward compatibility, see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/null
 console.log( myVariable + ": " + description );
-
-
-// Since ES6, there is also the type "Symbol", but it
-// should not be covered here.
