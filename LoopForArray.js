59b3f982fa55532ed2ba5484b59e6298f90c0976
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-09-10 12:55:25 +0200

    SchleifeFuerArray -> LoopForArray

---
+++ LoopForArray.js
@@ -0,0 +1,14 @@
+
+const myArray = [ "Duale", "Hochschule", "BW", "Karlsruhe" ];
+
+console.log();
+
+for ( let i = 0; i < myArray.length; i++ ) {
+
+    let word = myArray[i];
+    console.log( word );
+}
+
+console.log();
+
+
