a631e2cfd4a0091cfc9cf0b45642b4ed39fd1ac2
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-09-10 10:15:30 +0200

    Gleichheits-Operator translated to English

---
+++ EqualsOperator.js
@@ -0,0 +1,25 @@
+
+const fourAsString = "4";
+const fourAsNumber = 4;
+
+if ( fourAsString == fourAsNumber ) {
+
+    console.log( "\nComparison 1: Equals." );
+
+} else {
+
+    console.log( "Comparison 1: Not Equals." );
+}
+
+
+if ( fourAsString === fourAsNumber ) {
+
+    console.log( "Comparison 2: Equals.\n" );
+
+} else {
+
+    console.log( "\nComparison 2: Not Equals.\n" );
+}
+
+
+
