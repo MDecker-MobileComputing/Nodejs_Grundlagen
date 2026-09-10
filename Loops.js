f25ac161c520e67bc396ae50a0e4df071b70b200
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-09-10 11:01:43 +0200

    Translated Schleifen.js to Loops.js

---
+++ Loops.js
@@ -0,0 +1,31 @@
+
+console.log();
+
+// while loop
+let i1 = 1;
+while ( i1 <= 5 ) {
+
+    console.log( "i1=" + i1 );
+    i1++;
+}
+console.log();
+
+
+// do-while loop
+let i2 = 1;
+do {
+
+    console.log( "i2=" + i2 );
+    i2++;
+} while ( i2 <= 5 );
+console.log();
+
+
+// for loop
+for ( let i3 = 1; i3 <= 5; i3++ ) {
+
+    console.log( "i3=" + i3 );
+}
+console.log();
+
+
