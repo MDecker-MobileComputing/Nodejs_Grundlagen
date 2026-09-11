cf6fda896493d1d0342d6fd0e894984c7f444bcc
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-09-11 16:20:44 +0200

    Create SyntaxErrorDemo.js

---
+++ SyntaxErrorDemo.js
@@ -0,0 +1,9 @@
+
+/*
+ * Because of the syntax error in the last line, the whole file is not executed,
+ * so the lines before the line with the syntax error won't be shown.
+ */
+
+console.log( "\nThis line is syntactically correct." );
+
+console.log( "\nThis line is syntactically incorrect."
