93a47b656a2ad75b94a7c2d9f8278ba2a51a17cf
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-09-11 16:26:05 +0200

    new folgers: Loops, SyntaxErrors, functions

---
+++ SyntaxErrors/SyntaxErrorDemo-2.js
@@ -0,0 +1,16 @@
+/*
+ * This program is not executed because it contains a syntax error in the else block.
+ * Because of the syntax error in the last line, the whole file is not executed.
+ */
+
+
+const number = 123;
+
+if ( number > 100 ) {
+
+    console.log( "Number is greater than 100." );
+
+} else {
+
+    console.log( "Number is less than or equal to 100."
+}
